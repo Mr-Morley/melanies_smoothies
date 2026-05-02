4aaf6a61c1791887abae9b6ec1a537c4a2b20408
William Morley <75666512+Mr-Morley@users.noreply.github.com>
2026-05-02 23:25:17 +0200

    Fix session initialization in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 st.write("Choose the fruits you want in your smoothie!")
 
 cnx= st.connection("snowflake")
-session = cnx.session
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
